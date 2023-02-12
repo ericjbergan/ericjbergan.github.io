f624150b0abe36096a37c04fea93fd97c3f1f7f0
ericjbergan <ericjbergan@gmail.com>
2023-02-12 09:57:34 -0800

    Updates

---
+++ precache-manifest.4c677f3ad79df1c66e9aab6525d46c71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f3e9342e33e3b29789bc5b253a868e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74a850cd375d718d220b",
+    "url": "/static/css/main.7c7e1ac3.chunk.css"
+  },
+  {
+    "revision": "aace62d372223376f37d",
+    "url": "/static/js/2.bf331c17.chunk.js"
+  },
+  {
+    "revision": "74a850cd375d718d220b",
+    "url": "/static/js/main.adb82a18.chunk.js"
+  },
+  {
+    "revision": "3d9cbe3bfd1afa8e6c98",
+    "url": "/static/js/runtime~main.42f5c28b.js"
+  }
+]);
